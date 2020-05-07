413b3488a15cabc559b9082cd13b06074bc5d8a1
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-05-07 11:47:30 +0800

    更新代码

---
+++ study-canal-kafka/src/main/java/study/canal/kafka/listener/SimpleListener.java
@@ -0,0 +1,23 @@
+package study.canal.kafka.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class SimpleListener {
+
+    @KafkaListener(topics = {"topic"})
+    public void onMessage(ConsumerRecord<?, ?> record) {
+        Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+        if (kafkaMessage.isPresent()) {
+            Object message = kafkaMessage.get();
+            log.info("record= {}", record);
+            log.info("message= {}", message);
+        }
+    }
+}
